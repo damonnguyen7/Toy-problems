1db45caaf572095dbe008115a5549b4037c249c1
Damon <damonnguyen92@gmail.com>
2016-09-06 14:44:15 -0700

    Add example to clarify challenge

--- sumArray.js
+++ sumArray.js
@@ -1,5 +1,13 @@
 /* Given an array of numbers, calculate the greatest contiguous sum of numbers in it. 
-   A single array item will count as a contiguous sum. */
+   A single array item will count as a contiguous sum. 
+
+  Input: [1, 2, 3] Output: 6
+  Input: [4, -1, 5] Output: 8
+  Input: [10, -11, 11] Output: 11
+  Input: [1, 2, 3, -6, 4, 5, 6] Output: 15
+  
+*/
+
 /*
 //create parent function sumArray which take an input array.
   //create a function sumAll that will sum up all the number in the arguments array.
