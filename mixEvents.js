a6d0935175145cb6865d6b42dcf3b99920903f80
Damon <damonnguyen92@gmail.com>
2016-09-13 10:55:59 -0700

    Define mixEvents

---
+++ mixEvents.js
@@ -0,0 +1,42 @@
+/*
+Caveats:
+- mixEvents should return the original object it was passed after extending it.
+- If we repeatedly call .on with the same event name, it should
+continue to call the old function as well. That is to say, we can have multiple
+listeners for an event.
+- If obj.trigger is called with additional arguments, pass those to the
+listeners.
+- It is not necessary to write a way to remove listeners.
+
+Example usage:
+var obj = mixEvents({ name: 'Alice', age: 30 });
+obj.on('ageChange', function(){ // On takes an event name and a callback function
+  console.log('Age changed');
+});
+obj.age++;
+obj.trigger('ageChange'); // This should call our callback! Should log 'age changed'.
+
+*/
+
+var mixEvents = function(obj) {
+  obj.events = {};
+
+  obj.on = function(event, callback) {
+    obj.events[event] = obj.events[event] ? obj.events[event] : [];
+    if (obj.events[event].indexOf(callback) === -1) {
+      obj.events[event].push(callback);
+    }
+  }
+
+  obj.trigger = function(event) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    if (obj.events[event] !== undefined) {
+      obj.events[event].forEach(function(event) {
+        event.apply(this, args);
+      });
+    }
+  };
+  
+  return obj;
+}
+
