f4acbea705c3a2e26c835a3de6282be3612036e0
Damon <damonnguyen92@gmail.com>
2017-01-09 14:31:55 -0800

    Create test for sumLinkedList method

--- test/linkedlistSpec.js
+++ test/linkedlistSpec.js
@@ -17,13 +17,14 @@ describe('linkedList', function() {
     expect(LinkedList).to.have.property('tail');
   });
 
-  it('should have methods: addToTail, removeHead, removeNode, removeDuplicateNode, nthToLast, findBeginningLoop', function() {
+  it('should have methods: addToTail, removeHead, removeNode, removeDuplicateNode, nthToLast, findBeginningLoop, and sumLinkedList', function() {
     expect(LinkedList).to.have.property('addToTail');
     expect(LinkedList).to.have.property('removeHead');
     expect(LinkedList).to.have.property('removeNode');
     expect(LinkedList).to.have.property('removeDuplicateNode');
     expect(LinkedList).to.have.property('nthToLast');
     expect(LinkedList).to.have.property('findBeginningLoop');
+    expect(LinkedList).to.have.property('sumLinkedList');
   });
 
 });
@@ -300,4 +301,72 @@ describe('findBeginningLoop', function() {
     endOfLoop.next = startOfLoop;
     expect(LinkedList.findBeginningLoop(LinkedList.head)).to.equal('c');
   });
+});
+
+describe('sumLinkedList', function() {
+  var LinkedList;
+
+  beforeEach(function() {
+    LinkedList = new linkedList.LinkedList();
+  });
+
+  it('should be a function', function() {
+    expect(typeof LinkedList.sumLinkedList).to.equal('function');
+  });
+
+  it('should throw error if argument is not a linked list', function() {
+    var error = 'argument must be a linked list';
+
+    expect(LinkedList.sumLinkedList(LinkedList.head, 7)).to.equal(error);
+    expect(LinkedList.sumLinkedList(LinkedList.head, 'string')).to.equal(error);
+    expect(LinkedList.sumLinkedList(LinkedList.head, function(){})).to.equal(error);
+    expect(LinkedList.sumLinkedList(LinkedList.head, undefined)).to.equal(error);
+  });
+
+  it('should return a single linked list', function() {
+    var LinkedList2 = new linkedList.LinkedList();
+    expect(typeof LinkedList.sumLinkedList(LinkedList.head, LinkedList2.head)).to.equal('object');
+    expect(Array.isArray(LinkedList.sumLinkedList(LinkedList.head, LinkedList2.head))).to.equal(false);
+  });
+
+  it('should output linked list with the correct node values', function() {
+    var LinkedList = new linkedList.LinkedList();
+    var listOfLinkedListValues = [
+      [[0, 1, 2], [1, 1, 1]],
+      [[3, 1, 5], [5, 9, 2]],
+      [[9, 9, 9], [2, 2, 3]]
+    ]
+
+    var expectation = {
+      '0': {value: 1, next: {value: 2, next: {value: 3, next: null}}},
+      '1': {value: 8, next: {value: 0, next: {value: 8, next: null}}},
+      '2': {value: 1, next: {value: 2, next: {value: 3, next: {value: 1, next: null}}}}
+    }
+
+    function createLinkedLists(a, b) {      
+      var linkedList1 = new linkedList.LinkedList();
+      a.forEach(function(value) {
+        linkedList1.addToTail(value);
+      });
+
+      var linkedList2 = new linkedList.LinkedList();
+      b.forEach(function(value) {
+        linkedList2.addToTail(value);
+      });
+      // console.log('linkedList1.head: ', linkedList1.head);
+      // console.log('linkedList2.head: ', linkedList2.head);
+
+      // console.log('whole linked list: ', linkedList1);
+      var x = [linkedList1.head, linkedList2.head];
+      console.log('XXXXXXX: ',  x);
+      return [linkedList1, linkedList2];
+    }
+
+
+    var suite1 = createLinkedLists(listOfLinkedListValues[0][0], listOfLinkedListValues[0][1]);
+    expect(LinkedList.sumLinkedList(suite1[0].head, suite1[1].head)).to.deep.equal(expectation[0]);
+
+    
+  });
+
 });
