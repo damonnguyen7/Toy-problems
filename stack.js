c0b72de1697e1b946bb2d738cd248d273d0e7250
Damon <damonnguyen92@gmail.com>
2017-01-10 10:29:31 -0800

    define functions push pop and length

--- stack.js
+++ stack.js
@@ -1,11 +1,33 @@
 function Stack() {
   this.stack = {}
+  this.stackSize = 0;
 };
 
-Stack.prototype.push = function() {};
-Stack.prototype.pop = function() {};
-Stack.prototype.length = function() {};
-Stack.prototype.showStack = function() {};
+Stack.prototype.push = function(value) {
+  if (typeof value !== 'string' && typeof value !== 'number') {
+    return 'argument must be string or number';
+  }
+  this.stack[this.stackSize] = value;
+  this.stackSize++;
+  return this.stack;
+};
+
+Stack.prototype.pop = function() {
+  if (this.stackSize > 0) {
+    this.stackSize--
+    var popped_value = this.stack[this.stackSize];
+    delete this.stack[this.stackSize];
+    return popped_value;
+  } else {
+    this.stackSize = 0;
+    return 'no more item on stack!';
+  }
+};
+
+Stack.prototype.length = function() {
+  return this.stackSize;
+};
+
 
 
 module.exports = Stack;
