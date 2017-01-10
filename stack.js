4a8796a30521a875f7cb49cc9b336bfd100ee0c4
Damon <damonnguyen92@gmail.com>
2017-01-10 14:22:52 -0800

    Create SetOfStacks function constructor with properties and methods

--- stack.js
+++ stack.js
@@ -45,6 +45,38 @@ Stack.prototype.length = function() {
   return this.stackSize;
 };
 
+//################################SetOfStacks####################################
+function SetOfStacks() {
+  this.stack = {};
+  this.stackSize = 0;
+  this.threshold = 20;
+}
+
+SetOfStacks.prototype.push = function(stack) {
+  if (stack.length() > this.threshold) {
+    return 'argument stack must be less or equal to 20';
+  }
+  this.stack[this.stackSize] = stack;
+  this.stackSize++;
+};
+
+SetOfStacks.prototype.pop = function() {
+  if (this.stackSize > 0) {
+    this.stackSize--;
+    var poppedStack = this.stack[this.stackSize];
+    delete this.stack[this.stackSize];
+    return poppedStack;
+  } else {
+    this.stackSize = 0;
+    return 'no more item on stack!';
+  }
+};
+
+SetOfStacks.prototype.length = function() {
+  return this.stackSize;
+};
+
 module.exports = {
-  Stack: Stack
+  Stack: Stack,
+  SetOfStacks: SetOfStacks
 };
