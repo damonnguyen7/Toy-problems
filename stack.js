3a9fccf852633fd1413a4d64a57fe0751512da15
Damon <damonnguyen92@gmail.com>
2017-01-10 12:00:45 -0800

    Create Stack.prototype.min

--- stack.js
+++ stack.js
@@ -1,12 +1,24 @@
 function Stack() {
   this.stack = {}
   this.stackSize = 0;
+  this.stackMin = [];
+  this.currentMin = undefined;
 };
 
 Stack.prototype.push = function(value) {
+  //value must be a number or string
   if (typeof value !== 'string' && typeof value !== 'number') {
     return 'argument must be string or number';
   }
+  //define current minimum if undefined
+  if (this.currentMin === undefined) {
+    this.currentMin = value;
+  }
+
+  if (value < this.currentMin) {
+    this.currentMin = value;
+  }
+  this.stackMin.push(this.currentMin);
   this.stack[this.stackSize] = value;
   this.stackSize++;
   return this.stack;
@@ -14,6 +26,7 @@ Stack.prototype.push = function(value) {
 
 Stack.prototype.pop = function() {
   if (this.stackSize > 0) {
+    this.stackMin.pop();
     this.stackSize--
     var popped_value = this.stack[this.stackSize];
     delete this.stack[this.stackSize];
@@ -24,10 +37,14 @@ Stack.prototype.pop = function() {
   }
 };
 
+Stack.prototype.min = function() {
+  return this.stackMin[this.stackSize - 1];
+}
+
 Stack.prototype.length = function() {
   return this.stackSize;
 };
 
-
-
-module.exports = Stack;
+module.exports = {
+  Stack: Stack
+};
